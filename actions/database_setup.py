892965b8521106d166f5d64c9cd41fc849d7a800
Tarun-nano <nano.tarun@gmail.com>
2021-05-28 13:01:19 +0530

    db setup done

--- actions/database_setup.py
+++ actions/database_setup.py
@@ -3,14 +3,11 @@ from sqlalchemy import create_engine
 
 
 def ask_postgre():
-    host = input('\nPlease enter the host number: like: 127.0.0.1\n')
-    db_name = input('\nPlease enter the db name: like: master\n')
-    user = input('\nPlease enter the username: like: test_user\n')
-    password = input('\nPlease enter the password: like: password\n')
-    db_string = "postgres+psycopg2://{}:{}@{}/{}".format(user, password,
-                                                         host,
-                                                         db_name)
+
+    db_string = "postgres+psycopg2://username:passowrd@localhost/covid"#.format(user, password,host,db_name)
+    print("connecting to db")
     engine = create_engine(db_string)
+    print("engine connected")
     return engine
 
 
