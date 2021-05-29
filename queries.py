8834db59511f7155b00511f69e94756e67a7065b
jaineel dharia <jaineeldharia123@gmail.com>
2021-05-29 19:11:00 +0530

    Update queries.py

--- queries.py
+++ queries.py
@@ -10,11 +10,17 @@ if __name__ == '__main__':
 
 def abc():
     engine_client = database_setup.ask_postgre()
-    query = 'SELECT first_name FROM actor WHERE (actor_id = 1) '
+    query = 'SELECT first_name,last_name FROM actor WHERE (actor_id >= 1 and actor_id <=5) '
     df_client = pd.read_sql(query, engine_client)
-    temp = list(df_client.iloc[0])
+    first_name_list = list(df_client['first_name'])
+    last_name_list = list(df_client['last_name'])
+    n = len(first_name_list)
+    temp_array = []
+    for i in range(0,n):
+        add = first_name_list[i] + " " +  last_name_list[i]
+        temp_array.append(add)
 
-    return temp[0]
+    return temp_array
 
 
 #print(abc())
