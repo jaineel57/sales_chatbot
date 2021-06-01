38d06b3cc874f6157707b390c92819c97243703a
jaineel dharia <jaineeldharia123@gmail.com>
2021-06-01 21:21:44 +0530

    Update actions.py

--- actions/actions.py
+++ actions/actions.py
@@ -14,7 +14,7 @@ from rasa_sdk.executor import CollectingDispatcher
 import pandas as pd
 
 import json
-  
+
 # Opening JSON file
 f = open('karam_data_dump.json',)
 
@@ -47,7 +47,9 @@ class Actiontotal_sales(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
          val1 = str(imp['total_sales'])
-         dispatcher.utter_message(val1)
+         print(val1)
+         final_message = "The overall sales of the company are :" +" "+val1
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -61,6 +63,7 @@ class Actionchange_sales(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val2 = str(imp['change_sales'])
+         print(val2)
 
          dispatcher.utter_message(val2)
 
@@ -76,6 +79,7 @@ class Actionclients_info(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val3 = str(imp['clients_info'])
+         print(val3)
 
          dispatcher.utter_message(val3)
 
@@ -91,8 +95,10 @@ class Actionmax_clients_info(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val4 = str(imp['max_client_info'])
+         print(val4)
+         final_message = "The client who had highest sales is :" +" "+val4
 
-         dispatcher.utter_message(val4)
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -105,8 +111,10 @@ class Actionmax_customer_return_info(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val6 = str(imp['max_customer_return_info'])
+         print(val6)
+         final_message = "The custmer who had maximum returns is :" +" "+val6
 
-         dispatcher.utter_message(val6)
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -119,7 +127,9 @@ class Actionregion_product_info(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val7 = imp['region_product_info']
-         dispatcher.utter_message(val7)
+         print(val7)
+         final_message = "The region with highest sales is :" +" "+val7
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -132,7 +142,9 @@ class Actiontotal_returns(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val8 = str(imp['total_returns'])
-         dispatcher.utter_message(val8)
+         print(val8)
+         final_message = "The overall returns of the company :" +" "+val8
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -146,7 +158,9 @@ class Actionreturns_category_amount(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val9 = str(imp['returns_category_amount'])
-         dispatcher.utter_message(val9)
+         print(val9)
+         final_message = "The category with maximum returns in terms of amount is :" +" "+val9
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -159,7 +173,9 @@ class Actionreturns_category_quantity(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val10 = str(imp['returns_category_quantity'])
-         dispatcher.utter_message(val10)
+         print(val10)
+         final_message = "The maximum returns in terms of quantity are :" +" "+val10
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -173,7 +189,9 @@ class Actionyearly_sales_distribution_category(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val11 = str(imp['yearly_sales_distribution_category'])
-         dispatcher.utter_message(val11)
+         print(val11)
+         final_message = "Total yearly sales are :" +" "+val11
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -186,7 +204,9 @@ class Actionmonthly_sales_distribution_category(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val12 = str(imp['monthly_sales_distribution_category'])
-         dispatcher.utter_message(val12)
+         print(val12)
+         final_message = "Total monthly sales are :" +" "+val12
+         dispatcher.utter_message(final_message)
 
          return []
 
@@ -200,6 +220,8 @@ class Actiondealer_sales_info(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
          val13 = str(imp['dealer_sales_info'])
-         dispatcher.utter_message(val13)
+         print(val13)
+         final_message = "The dealer with highest sales of products is :" +" "+val13
+         dispatcher.utter_message(final_message)
 
          return []
